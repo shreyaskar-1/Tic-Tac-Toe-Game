f4c0978877a1f38dd5549b6eb613f59a677e85c5
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-02-22 15:38:28 +0530

    addons

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetgame = document.querySelector("#resetgame");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -23,5 +26,28 @@ boxes.forEach((box) => {
             box.innerText = "X";
             turnO = true;
         }
+        box.disabled= true;
+        checkWinner();
     });
 });
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+};
+
+const checkWinner = () => {
+    for (let pattern of winPattern) {
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val !== "" && pos2val !== "" && pos3val !== "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                console.log("winner");
+                showWinner(pos1val);
+            }
+        }
+    }
+};
+
